c81b460e89a818e44213456da6fdbd2e8069bfe9
Shubhangi <shubhangi@teewe.in>
2017-04-04 19:13:39 +0530

    added feature for fetching local weather

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-
 // call this function on DOM ready
 $(document).ready(function(){
 
-	// on submit weather click call this function
-	$("#submitWeather").click(function(){
+	// on displayLocalData click call this function
+	$("#clickLocalWeather").click(function(){
+		var openWeatherMap = 'http://api.openweathermap.org/data/2.5/weather'
+		if (window.navigator && window.navigator.geolocation) {
+		    window.navigator.geolocation.getCurrentPosition(function(position) {
+		        $.getJSON(openWeatherMap, {
+		            lat: position.coords.latitude,
+		            lon: position.coords.longitude,
+		            units: 'metric',
+		            APPID: 'a5cc950df1a1ee711cf9a6e2e4962ba4'
+		        }).done(function(data) {
+	        		var info = show(data);
+					$("#displayLocalData").html(info);
+	        	})
+	    	})
+		}
+	});
+
+	// on submitCity click call this function
+	$("#submitCity").click(function(){
 
 		// store the value of the input field in a variable
 		var city = $("#city").val();
@@ -14,22 +31,18 @@ $(document).ready(function(){
 			// will be making an AJAX request can use also $.getJSON()
 			// in this ex will be using $.ajax()
 
-			$.ajax({
+			
 
+			$.getJSON({
 				// takes in some parameters
 				url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&APPID=a5cc950df1a1ee711cf9a6e2e4962ba4" ,
-				type: "GET",
-				dataType: "jsonp",
-				success: function(data){
+			}).done(function(data){
 					var info = show(data);
-
 					$("#displayData").html(info);
 
 					// code for clearing the input fields
 					$("#city").val("");
-				}
-
-			});
+				})
 
 		}else{
 			$("#error").html("Cannot be empty");
@@ -38,7 +51,8 @@ $(document).ready(function(){
 });
 
 function show(data){
-	return "<p><b>Temprature </b>" + data.main.temp + " &degC</p>" + 
+	return "<p><b>Location </b>" + data.name + "</p>" + 
+		   "<p><b>Temprature </b>" + data.main.temp + " &degC</p>" + 
 		   "<p><b>Outside </b>" + data.weather[0].description + "</p>" +
 		   "<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png'>"		   
 }
